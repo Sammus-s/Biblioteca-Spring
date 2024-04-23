4e79916a6bf772d5fbb4e33f71614ed071fed162
Sammus-s <samuel.rodrigues2004@hotmail.com>
2024-04-22 21:46:07 -0300

    Inicializado serviço Livro

---
+++ src/main/java/com/example/livro/LivroApplication.java
@@ -0,0 +1,13 @@
+package com.example.livro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/livro/LivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
