0dea1aae9072a850a4a46f1aeeb53b5044984f12
Sammus-s <samuel.rodrigues2004@hotmail.com>
2024-05-13 21:41:13 -0300

    Criado método deletar para remover os registros do banco

--- src/main/java/com/example/livro/controller/LivroController.java
+++ src/main/java/com/example/livro/controller/LivroController.java
@@ -1,19 +1,18 @@
 package com.example.livro.controller;
 
 import com.example.livro.entity.Livro;
-import com.example.livro.enums.Categoria;
-import com.example.livro.enums.Modelo;
 import com.example.livro.repository.LivroRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class LivroController {
+    //TODO refatorar
+
     @Autowired
     private LivroRepository livroRepository;
 
@@ -36,5 +35,12 @@ public class LivroController {
     @RequestMapping(path = "/livros", method = RequestMethod.PUT)
     public void atualizar(@RequestBody Livro livro){
         livroRepository.save(livro);
+    }
+
+    @ResponseBody
+    @Transactional
+    @RequestMapping(path = "/livros/{isbn}", method = RequestMethod.DELETE)
+    public void deletar(@PathVariable Long isbn) {
+        livroRepository.deleteById(isbn);
     }
 }
